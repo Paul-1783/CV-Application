80f4f81f21ab586c196017c8687591fecef1b003
Paul-1783 <paulschmitt144@gmail.com>
2026-02-27 16:41:31 +0100

    established general struture of website

---
+++ src/components/tabsContainer/tabsContainer.jsx
@@ -0,0 +1,10 @@
+import "./tabsContainer.css";
+
+export function TabsContainer() {
+  return (
+    <span className="tabs-container">
+      <h1>CV-Creator</h1>
+      <section className="tabs-interior"></section>
+    </span>
+  );
+}
