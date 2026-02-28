00359db7f7f16de7daf254fd665fc4a7b1c0695f
Paul-1783 <paulschmitt144@gmail.com>
2026-02-28 14:30:02 +0100

    added hover info to buttons

--- src/components/tabsContainer/tabsContainer.jsx
+++ src/components/tabsContainer/tabsContainer.jsx
@@ -7,13 +7,35 @@ import projectIcon from "./../../assets/project.png";
 import publicationIcon from "./../../assets/publication.png";
 import skillIcon from "./../../assets/skill.png";
 import linkIcon from "./../../assets/links.png";
+import generalIcon from "./../../assets/general.png";
+
+const sectionNames = [
+  "general info",
+  "education",
+  "experience",
+  "language",
+  "project",
+  "publication",
+  "skills",
+  "links",
+];
 
 export function TabsContainer() {
+  let counter = 0;
+  let buttons = document.querySelectorAll(".sideMenuButton");
+  buttons.forEach((sideMenuButton) => {
+    sideMenuButton.setAttribute("section-name", sectionNames[counter]);
+    counter += 1;
+  });
+
   return (
     <span className="tabs-container">
       <h1>CV-Creator</h1>
       <main className="tabs-interior">
         <div id="side-menu">
+          <button className="sideMenuButton">
+            <img src={generalIcon} alt="general" />
+          </button>
           <button className="sideMenuButton">
             <img src={educationIcon} alt="education" />
           </button>
