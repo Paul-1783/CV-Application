36728ae50b8f0b716c55c6fb1b3384fa3eee4cde
Paul-1783 <paulschmitt144@gmail.com>
2026-04-21 00:36:10 +0200

    fixed false hover effect on form buttons

--- src/components/sideButton/sideButton.jsx
+++ src/components/sideButton/sideButton.jsx
@@ -1,7 +1,7 @@
  
 export function SideMenuButton({clickedBtnIndex, info, handleClick, newClassName}) {
      return(
-         <button className={"sideMenuButton " + newClassName} name={info.sectionName}  onClick={e => {
+         <button className={"sideMenuButton " + newClassName} name={info.sectionName} style={newClassName === "clicked-btn" ? {boxShadow: "none"} : {}}  onClick={e => {
             const clickInfo = {...e, clickedBtnIndex: clickedBtnIndex }
              handleClick(clickInfo)}}>
             <img src={info.icon} alt={info.sectionName}  />
